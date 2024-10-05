1db3d78b7063c9850ea0b120baa974945b3ab597
luccacastro <luccacastro1008@poli.ufrj.br>
2024-10-05 17:34:50 -0300

    Adicionando interação do modelo do robô com o chão

--- main.m
+++ main.m
@@ -1,8 +1,8 @@
 
 % Posição do centro de massa do robô
-cx = 10;
-cy = 15;
-cz = 20;
+cx = 1;
+cy = 5;
+cz = 2;
 pos = [cx, cy, cz];
 
 % Angulação do centro de massa do robô
@@ -12,10 +12,10 @@ yaw   = 0;
 rpy = [roll, pitch, yaw];
 
 % Configuração das juntas da perna
-q_leg1 = [0, pi/6, -pi/2];
-q_leg2 = [0, pi/6, -pi/6];
-q_leg3 = [0, -pi/6, pi/6];
-q_leg4 = [0, -pi/6, pi/6];
+q_leg1 = [pi/6, 0, -pi/6];
+q_leg2 = [pi/6, 0, -pi/6];
+q_leg3 = [-pi/6, 0, pi/6];
+q_leg4 = [-pi/6, 0, pi/6];
 
 % Cálculo da Cinemática Direta das 4 patas do robô
 [pos_leg_1, pos_leg_2, pos_leg_3, pos_leg_4] = new_foward_kinematics(pos, rpy, q_leg1, q_leg2, q_leg3, q_leg4);
