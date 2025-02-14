0637c51a93b1692d3daab3473eeeb6dbaf389c5f
luccacastro <luccacastro1008@poli.ufrj.br>
2025-02-13 23:43:42 -0300

    ajustando modelo 3D para ser um modelo dinamico

--- main.m
+++ main.m
@@ -1,5 +1,7 @@
 constants
 
+%TODO: Angle constraint no modelo simulink
+
 % Posição do centro de massa do robô
 cx = 0;
 cy = 0;
@@ -13,10 +15,10 @@ yaw   = 0;
 rpy = [roll; pitch; yaw]; %TODO: adicionar o rpy na cinemática inversa (arg 'rpy')
 
 % Configuração das juntas da perna
-q_leg1 = [0; 0; -pi/6]
-q_leg2 = [0; 0; -pi/6]
-q_leg3 = [0; 0; +pi/6]
-q_leg4 = [0; 0; +pi/6]
+q_leg1 = [0; pi/6; -pi/3];
+q_leg2 = [0; pi/6; -pi/3];
+q_leg3 = [0; -pi/6; +pi/3];
+q_leg4 = [0; -pi/6; +pi/3];
 
 % Cálculo da Cinemática Direta das 4 patas do robô
 [pos_leg_1, pos_leg_2, pos_leg_3, pos_leg_4] = foward_kinematics(pos, rpy, q_leg1, q_leg2, q_leg3, q_leg4)
@@ -26,4 +28,3 @@ q_leg4 = [0; 0; +pi/6]
 
 % Validação da Cinemática Inversa
 [new_pos_leg_1, new_pos_leg_2, new_pos_leg_3, new_pos_leg_4] = foward_kinematics(pos, rpy, new_q_leg1, new_q_leg2, new_q_leg3, new_q_leg4)
-
