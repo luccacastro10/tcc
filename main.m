f6d1d474e1eabfcde9e520700e63e6d5b9a839c4
luccacastro <luccacastro1008@poli.ufrj.br>
2025-04-27 22:02:59 -0300

    adicionando primeira versão do controle de altura

--- main.m
+++ main.m
@@ -3,11 +3,12 @@ step_constants
 
 %TODO: Angle constraint no modelo simulink
 %TODO: Avaliar como a transformação homogênea TIC será tratada posteriormente
+%TODO: Ler C e L dinamicamente no matlab function block
 
 % Configuração das juntas da perna
-q_leg1 = [+pi/4; +pi/6; -pi/3];
+q_leg1 = [+pi/9; +pi/6; -pi/3];
 q_leg2 = [+pi/9; +pi/6; -pi/3];
-q_leg3 = [-pi/4; -pi/6; +pi/3];
+q_leg3 = [-pi/9; -pi/6; +pi/3];
 q_leg4 = [-pi/9; -pi/6; +pi/3];
 
 % Cálculo da Cinemática Direta das 4 patas do robô
@@ -26,3 +27,10 @@ leg1.jacob0(q_leg1);
 
 pos_leg_1
 new_pos_leg_1
+
+last_roll = 0;
+last_pitch = 0;
+last_yaw = 0;
+last_u_roll = 0;
+last_u_pitch = 0;
+last_u_yaw = 0;
